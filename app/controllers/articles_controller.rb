faf6c69f3f1c127b6d76a19711cf99679923e79b
kanazawayu <kanazawa975@gmail.com>
2020-04-14 21:57:30 +0900

    2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,44 @@
 class ArticlesController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index, :show]
   
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(article_params)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comments = @article.comments.includes(:user)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :image, :body).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
   devise_for :users
-  resources :articles, only: :index
+  resources :articles do
+    resources :comments, only: :create
+  end
 end
