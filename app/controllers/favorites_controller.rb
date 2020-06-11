e7244bc2e5362b92378fb87f3ec71d4c9b76affa
kanazawayu <kanazawa975@gmail.com>
2020-06-11 20:55:50 +0900

    favorite-page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   before_action :set_article, only: [:create, :destroy]
+  before_action :move_to_index, only: [:index]
 
   def index
-    @article = Article.all
-    @favorite = Favorite.where(user_id: current_user.id)
-    # @articles = @article.favorites.where(user_id: current_user.id)
-    # @article = article.where.not(user_id: current_user.id)
+    user = User.find(current_user.id)
+    @name = user.name
+    @articles = Article.all.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def create
@@ -20,5 +20,9 @@ class FavoritesController < ApplicationController
   private
   def set_article
     @article = Article.find(params[:article_id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
   end
 end
